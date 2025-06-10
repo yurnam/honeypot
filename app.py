996cffc3817a0789d054973a56674855e52c136c
martin <m.kritzer@gmx.at>
2025-06-10 11:33:55 +0200

    test

--- app.py
+++ app.py
@@ -1,12 +1,13 @@
 from flask import Flask
+from random import randint
+from os import urandom
+app = Flask(name)
 
-app = Flask(__name__)
+@app.route('/<path:path>', methods=['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS', 'HEAD'])
+@app.route('/', defaults={'path': ''}, methods=['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS', 'HEAD'])
+def catch_all(path):
+    randomdata = urandom(randint(10, 110))
+    randomdata = randomdata.decode('utf-8', errors='ignore')
 
+    return randomdata, randint(100, 999)
 
-@app.route('/')
-def hello_world():  # put application's code here
-    return 'Hello World!'
-
-
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=80)
